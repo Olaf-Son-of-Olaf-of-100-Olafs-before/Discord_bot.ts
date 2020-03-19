f39a1cef8ea0b002c97f94919a5967384296985e
theinsanepenguin <36058090+chad-chadason-chad@users.noreply.github.com>
2020-03-18 21:51:49 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let config = {
-    "token": "NDUzNjM1Mzg0NTYwMDU4MzY5.XTHh-g.qyNI1NWjpD12UxL67_ojCYI765Q",
+    "token": "ENTER YOUR TOKEN",
     "prefix": "?",
     "commands": [
         "testCommand"
